dd23507c7d2c76f884761fdd71a2512782dfb2e7
Owlyraph <70176950+owlyraph@users.noreply.github.com>
2024-05-22 12:34:01 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,93 +1,48 @@
 // Create web server
-// npm install express
-// npm install body-parser
-// npm install mongoose
-// npm install ejs
-// npm install express-session
-// npm install connect-flash
-// npm install method-override
-// npm install passport
-// npm install passport-local
-// npm install passport-local-mongoose
-// npm install express-sanitizer
-// npm install moment
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsPath = path.join(__dirname, 'comments.json');
 
-// Require packages
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    Comment = require("./models/comment"),
-    seedDB = require("./seeds"),
-    methodOverride = require("method-override");
-
-// Connect to the database
-mongoose.connect("mongodb://localhost/comments");
-
-// Set up the app
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-
-// Seed the database
-seedDB();
+// Middleware
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 // Routes
-app.get("/", function(req, res) {
-    res.redirect("/comments");
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
-// INDEX
-app.get("/comments", function(req, res) {
-    Comment.find({}, function(err, comments) {
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsPath, 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      var comments = JSON.parse(data);
+      comments.push(req.body);
+      fs.writeFile(commentsPath, JSON.stringify(comments), function(err) {
         if (err) {
-            console.log(err);
+          console.error(err);
+          res.sendStatus(500);
         } else {
-            res.render("index", {comments: comments});
+          res.sendStatus(201);
         }
-    });
+      });
+    }
+  });
 });
 
-// NEW
-app.get("/comments/new", function(req, res) {
-    res.render("new");
+// Start server
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
 });
-
-// CREATE
-app.post("/comments", function(req, res) {
-    Comment.create(req.body.comment, function(err, comment) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/comments");
-        }
-    });
-});
-
-// SHOW
-app.get("/comments/:id", function(req, res) {
-    Comment.findById(req.params.id, function(err, comment) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("show", {comment: comment});
-        }
-    });
-});
-
-// EDIT
-app.get("/comments/:id/edit", function(req, res) {
-    Comment.findById(req.params.id, function(err, comment) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("edit", {comment: comment});
-        }
-    });
-});
-
-// UPDATE
-app.put("/comments/:id", function(req, res) {
-    Comment.findByIdAndUpdate(req.params.id, req.body.comment, function(err, comment) {
-        if (
